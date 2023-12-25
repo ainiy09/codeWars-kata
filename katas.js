2a4f5c8a1620848368ed3a2c73495d93bf1bf093
Nikita <100791162+ainiy09@users.noreply.github.com>
2023-12-25 21:17:41 +0300

    "Add new kata"
    "Add new kata"

--- katas.js
+++ katas.js
@@ -267,3 +267,30 @@ function primeFactors(n) {
   }
   return result;
 }
+// ________________________________________________________________________________________________________________
+// TASK:
+// Your job is to write a function which increments a string, to create a new string.
+
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+
+// foo -> foo1
+// foobar23 -> foobar24
+// foo0042 -> foo0043
+// foo9 -> foo10
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+// Solution:
+function incrementString (strng) {
+  const match = strng.match(/(.*?)(\d*)$/);
+  const prefix = match[1] || '';
+  const numStr = match[2] || '0';
+
+  const num = parseInt(numStr) + 1;
+  const incrementedNumStr = String(num).padStart(numStr.length, '0');
+
+  return prefix + incrementedNumStr;
+}
