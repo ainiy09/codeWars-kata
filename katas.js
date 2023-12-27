df9dc5abdd0d77ad78adc30b1282f7e321e685bd
Nikita <100791162+ainiy09@users.noreply.github.com>
2023-12-27 22:03:33 +0300

    "Add new kata"
    "Add new kata"

--- katas.js
+++ katas.js
@@ -311,3 +311,25 @@ function incrementString (strng) {
 function findOdd(A) {
   return A.reduce((result, num) => result ^ num, 0);
 }
+// ________________________________________________________________________________________________________________
+// TASK:
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+// The first word within the output should be capitalized only 
+// if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). 
+// The next words should be always capitalized.
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+// "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+// Solution:
+function toCamelCase(str) {
+  const words = str.split(/[-_]/);
+
+  const camelCaseString = words.map((word, index) =>
+    index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  ).join('');
+
+  return camelCaseString;
+}
