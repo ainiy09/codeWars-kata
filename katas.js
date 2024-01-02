c873c2be521eba0d7d8ff4989a7d1172eba85086
Nikita <100791162+ainiy09@users.noreply.github.com>
2024-01-02 18:41:28 +0300

    "Add new kata"
    "Add new kata"

--- katas.js
+++ katas.js
@@ -347,3 +347,17 @@ function toCamelCase(str) {
 function arrayDiff(a, b) {
   return a.filter(item => !b.includes(item));
 }
+// ________________________________________________________________________________________________________________
+// TASK:
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+// which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+// Solution:
+function persistence(num) {
+  return num < 10 ? 0 : 1 + persistence([...num.toString()].reduce((acc, digit) => acc * digit, 1));
+}
